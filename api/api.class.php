1b783689f0948b5cb2e455700e2960175bd0f63b
QiuMo <2625053283@qq.com>
2021-08-25 10:20:03 +0800

    一些新功能

---
+++ api/api.class.php
@@ -0,0 +1,98 @@
+<?php
+/**
+ * 聚合api
+ */
+class api {
+    public $key="";
+	/**
+	 * 获取帖子ID
+	 * @param {Object} $posturl 帖子链接
+	 */
+	public function getPostId($posturl){
+		//格式验证
+		if(empty($posturl)){
+			return array('code'=>500,'msg'=>'帖子链接不能为空');
+		}
+		if(strstr($posturl,'bbs.huluxia.com')==''){
+			return array('code'=>500,'msg'=>'帖子链接不正确');
+		}
+		if(strstr($posturl,'http://')=='' && strstr($posturl,'https://')==''){
+			return array('code'=>500,'msg'=>'帖子链接需包含http://或https://');
+		}
+		$res=file_get_contents($posturl);
+		preg_match_all('<input type="hidden" value="(.*?)" id ="postid"/>',$res, $postids);
+		$postid = $postids[1][0];
+		if(empty($postid)){
+			return array('code'=>500,'msg'=>'很抱歉该帖子已经删除');
+		}
+		return array('code'=>200,'msg'=>$postid);
+	}
+	/**
+	 * 获取用户头像
+	 * @param {Object} $posturl 帖子链接
+	 */
+	public function getAvatar($userid){
+		//格式验证
+		$url = "http://floor.huluxia.com/user/info/ANDROID/2.1?_key=".$this->key."&user_id=".$userid; //api
+        $res = $this->get_url($url);
+        $res = json_decode($res, true);
+		return $res['avatar'];
+	}
+	
+	/**
+	 * 获取关注前十
+	 * @param {Object} $userid 用户id
+	 */
+	public function getFriendship($userid){
+		//格式验证
+		if(empty($userid)){
+			return array('code'=>500,'msg'=>'用户ID不可为空');
+		}
+		$url = "http://floor.huluxia.com/friendship/following/list/ANDROID/2.0?user_id=".$userid."&count=10"; //api
+        $res = json_decode($this->get_url($url), true);
+        if(empty($res)){
+			return array('code'=>500,'msg'=>'未知异常');
+		}
+		if(count($res['friendships'])==0){
+		    return array('code'=>500,'msg'=>'关注列表为空或用户ID不存在');
+		}
+		return array('code'=>200,'msg'=>$res['friendships']);
+	}
+	
+	/**
+	 * 获取收藏前十
+	 * @param {Object} $userid 用户id
+	 */
+	public function getFavorite($userid){
+		//格式验证
+		if(empty($userid)){
+			return array('code'=>500,'msg'=>'用户ID不可为空');
+		}
+		$url = "http://floor.huluxia.com/post/favorite/list/ANDROID/2.0?count=10&user_id=".$userid; //api
+	    $res = json_decode($this->get_url($url), true);
+	    if(empty($res)){
+			return array('code'=>500,'msg'=>'未知异常');
+		}
+		if(count($res['posts'])==0){
+		    return array('code'=>500,'msg'=>'收藏列表为空或用户ID不存在');
+		}
+		return array('code'=>200,'msg'=>$res['posts']);
+	}
+	
+	/**
+	 * 通过URL获取页面信息
+	 * @param $url 地址
+	 * @return mixed 返回页面信息
+	 */
+	function get_url($url)
+	{
+	    $ch = curl_init();
+	    curl_setopt($ch, CURLOPT_URL, $url); //设置访问的url地址
+	    curl_setopt($ch, CURLOPT_SSL_VERIFYPEER, 0);
+	    curl_setopt($ch, CURLOPT_SSL_VERIFYHOST, 0);
+	    curl_setopt($ch, CURLOPT_RETURNTRANSFER, 1); //不输出内容
+	    $result = curl_exec($ch);
+	    curl_close($ch);
+	    return $result;
+	}
+}
---
+++ api/postid.php
@@ -0,0 +1,69 @@
+<?php
+header('Content-Type: text/html;charset=utf-8');
+header('Access-Control-Allow-Origin:*'); // *代表允许任何网址请求
+header('Access-Control-Allow-Methods:POST,GET,OPTIONS,DELETE'); // 允许请求的类型
+header('Access-Control-Allow-Credentials: true'); // 设置是否允许发送 cookies
+header('Access-Control-Allow-Headers: Content-Type,Content-Length,Accept-Encoding,X-Requested-with, Origin'); // 设置允许自定义请求头的字段
+include('api.class.php');//创建实例
+$api= new api();
+//获取调用的接口
+$action=$_REQUEST['action'];
+//获取帖子id
+if($action=='getpostid'){
+	$posturl=$_REQUEST['url'];
+	$res=$api->getPostId($posturl);
+	ajaxReturn($res);
+}
+//获取用户头像
+if($action=='getavatar'){
+	$userid=$_REQUEST['userid'];
+	$res=$api->getAvatar($userid);
+	showImg($res);
+}
+//获取关注前十
+if($action=='getfriendship'){
+    $userid=$_REQUEST['userid'];
+	$res=$api->getFriendship($userid);
+	ajaxReturn($res);
+}
+//获取收藏前十
+if($action=='getfavorite'){
+    $userid=$_REQUEST['userid'];
+	$res=$api->getFavorite($userid);
+	ajaxReturn($res);
+}
+
+//其他异常信息
+ajaxReturn(array('code'=>500,'msg'=>'请检查接口参数'));
+//输出json格式的返回值
+function ajaxReturn($array){
+    $content=json_encode($array,JSON_UNESCAPED_UNICODE);
+    if(empty($_GET['callback'])){
+        echo $content;
+		exit;
+    }else{
+        echo $_GET['callback']."(".$content.")";
+		exit;
+    }
+}
+
+//输出图片
+function showImg($img){
+    $info = getimagesize($img);
+    $imgExt = image_type_to_extension($info[2], false);  //获取文件后缀
+    $fun = "imagecreatefrom{$imgExt}";
+    $imgInfo = $fun($img);                  //1.由文件或 URL 创建一个新图象。如:imagecreatefrompng ( string $filename )
+    //$mime = $info['mime'];
+    $mime = image_type_to_mime_type(exif_imagetype($img)); //获取图片的 MIME 类型
+    header('Content-Type:'.$mime);
+    $quality = 100;
+    if($imgExt == 'png') $quality = 9;      //输出质量,JPEG格式(0-100),PNG格式(0-9)
+    $getImgInfo = "image{$imgExt}";
+    $getImgInfo($imgInfo, null, $quality);  //2.将图像输出到浏览器或文件。如: imagepng ( resource $image )
+    imagedestroy($imgInfo);
+    exit;
+}
+
+
+
+
